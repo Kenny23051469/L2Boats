a55d085266c0d9d9045e3db15773fbb65f44e486
Kenny23051469 <105729466+Blueyyyyy@users.noreply.github.com>
2024-10-24 23:43:25 +0800

    Changed the layout of the Boat.js to match the one in the exercise.

--- app/(tabs)/components/Boat.js
+++ app/(tabs)/components/Boat.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { ScrollView, Text, Image } from 'react-native';
+import { ScrollView, View, Text, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome6';
 
 function Boat ({ name, description, icon_name, poster }) {
     return (
         <ScrollView>
-            <Image source={poster} style={{width: 400, height: 500}} />
-            <Text> {name} </Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <Icon name={icon_name} size={20} style={{ marginLeft: 10 }} />
+                <Text style={{ fontWeight: 'bold', fontSize: 18 }}> {name} </Text>
+            </View>
             <Text> {description} </Text>
-            <Icon name = {icon_name} />
+            <Image source={poster} style={{ width: 400, height: 500 }} />
         </ScrollView>
     );
 }
-
 
 export default Boat;
--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -8,42 +8,42 @@ const AllBoats = () => {
             <Boat
                 name = "Sea Ray 500 Sundancer"
                 description = "Contoured lines and dramatic styling reveal a refined and powerful presence that will take your breath away."
-                icon_name = "film"
+                icon_name = "sailboat"
                 poster = {require('./img/sea_ray.jpg')}
             />
 
             <Boat
-                Name = "Four Winns Horizon 180"
+                name = "Four Winns Horizon 180"
                 description = "A sporty look and refined details truly set the Horizon 180 above all others."
-                icon_name = "film"
+                icon_name = "sailboat"
                 poster = {require('./img/four_winns.jpg')}
             />
 
             <Boat
-                Name = "Flipper 640 ST"
+                name = "Flipper 640 ST"
                 description = "A modern take on the classic, traditional hardtop and perfect for a family picnic."
-                icon_name = "film"
+                icon_name = "sailboat"
                 poster = {require('./img/flipper.jpg')}
             />
 
             <Boat
-                Name = "Princess V48"
+                name = "Princess V48"
                 description = "There is the option for an open design with a full-length cockpit and sunroof, or the enclosed deck saloon model, available with the option of a climate controlled interior."
-                icon_name = "film"
+                icon_name = "sailboat"
                 poster = {require('./img/princess.jpg')}
             />
 
             <Boat
-                Name = "Bayliner 175 Bowrider"
+                name = "Bayliner 175 Bowrider"
                 description = "Its outboard power gives you increased cockpit space and quiet, fuel-efficient performance."
-                icon_name = "film"
+                icon_name = "sailboat"
                 poster = {require('./img/bayliner.jpg')}
             />
 
             <Boat
-                Name = "Fairline Targa 47"
+                name = "Fairline Targa 47"
                 description = "Stretch out on the large sun bed aft while friends lounge in the generously appointed cockpit."
-                icon_name = "film"
+                icon_name = "sailboat"
                 poster = {require('./img/fairline.jpg')}
             />
         </ScrollView>
